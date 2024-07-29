955fb34a41814480f8d3caf555266867d9805878
cjabe <adrinbershik.2201008@srec.ac.in>
2024-07-29 18:37:41 +0530

    added config dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 
 mongoose.set("debug", true); // Enable detailed logging
 
-mongoose
-  .connect("mongodb://127.0.0.1:27017/clinic", {
-    serverSelectionTimeoutMS: 10000, // Timeout after 10s instead of 30s
-    connectTimeoutMS: 10000, // Timeout after 10s instead of no timeout
-  })
-  .then(() => {
+const dbConnection = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/clinic", {
+      serverSelectionTimeoutMS: 10000, // Timeout after 10s instead of 30s
+      connectTimeoutMS: 10000, // Timeout after 10s instead of no timeout
+    });
     console.log("Connected to MongoDB database!");
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error("Connection failed!", error);
-  });
+  }
+};
+
+module.exports = dbConnection;
